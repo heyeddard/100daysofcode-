e7e7fc239c975664d9daf8eaf5f46648340f9d1b
Sugam Agarwal <51432778+heyeddard@users.noreply.github.com>
2020-06-15 09:26:50 +0530

    Day 20 :- Solved into the tutorial Problem on HackerRank.

---
+++ into-tutorial-sorting-haccerrank.day-20.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int v;
+    cin>>v;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+       cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==v)
+        {
+            cout<<i;
+            break;
+        }
+    }
+}
