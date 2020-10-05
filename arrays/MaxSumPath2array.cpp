fb46ad6124317bf753d998099b8730e7afe127b2
Sugam Agarwal <51432778+heyeddard@users.noreply.github.com>
2020-10-05 23:22:46 +0530

    Create MaxSumPath2array.cpp

---
+++ arrays/MaxSumPath2array.cpp
@@ -0,0 +1,36 @@
+int max_path_sum(int A[], int B[], int l1, int l2)
+{
+    
+    int result = 0, sum1 = 0, sum2 = 0;
+    int i =0, j=0;
+    
+    while(i<l1 && j<l2)
+    {
+        
+        if(A[i] < B[j])
+            sum1 += A[i++];
+        else if(A[i] > B[j])
+            sum2 += B[j++];
+        else
+        {
+            result += max(sum1,sum2);
+            
+            sum1 = sum2 = 0;       
+            
+            while(i<l1 && j<l2 && A[i]==B[j])
+            {
+                result += A[i++];   
+                j++;
+            }
+        }
+    }
+    while (i < l1)
+        sum1  +=  A[i++];
+
+    while (j < l2)
+        sum2 +=  B[j++];
+
+    result +=  max(sum1, sum2);
+
+    return result;
+}
